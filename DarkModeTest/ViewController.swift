bbd8109cf01e90e03d38b776953153ccf8b598b2
Dimanikin <driver-s@mail.ru>
2019-09-05 15:24:09 +0200

    Initial Commit

---
+++ DarkModeTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DarkModeTest
+//
+//  Created by Dima on 05/09/2019.
+//  Copyright © 2019 Dima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
